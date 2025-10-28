743ef7eb8240f28d534928d161738a63f565936c
Owen <stefanescuowen@gmail.com>
2025-10-28 10:15:53 -0400

    Added the other documents needed for this portfolio. Added styling for each as well. Made sure the hamburger menu works on all documents.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('navLinks');
 
+if (hamburger && navLinks){
 hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
     navLinks.classList.toggle('active');
 });
+}
